0e40faea73d47570d4e65877b6290901bfb46c16
Jonas Hermsmeier <jhermsmeier@gmail.com>
2016-03-31 15:10:41 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# `git-branch-select`
+[![npm](http://img.shields.io/npm/v/git-branch-select.svg?style=flat-square)](https://npmjs.com/git-branch-select)
+[![npm license](http://img.shields.io/npm/l/git-branch-select.svg?style=flat-square)](https://npmjs.com/git-branch-select)
+[![npm downloads](http://img.shields.io/npm/dm/git-branch-select.svg?style=flat-square)](https://npmjs.com/git-branch-select)
+
+Interactive (local) branch selection in your repositories.
+
+## Install via [npm](https://npmjs.com)
+
+```sh
+$ npm install --global git-branch-select
+```
+
+## Usage
+
+```sh
+$ git branch-select
+# Alias it in your .gitconfig for added convenience,
+# for example: bs = branch-select
+```
+
+```
+~/C/node-bencode (bignum-support) $ git branch-select
+? Select branch (Use arrow keys)
+❯ bignum-support
+  feature/streams
+  housekeeping
+  master
+```
---
+++ index.js
@@ -0,0 +1,53 @@
+#!/usr/bin/env node
+var exec = require( 'child_process' ).exec
+var inquirer = require( 'inquirer' )
+var lines = []
+
+exec( 'git branch --no-color', function( error, stdout, stderr ) {
+
+  lines = stdout.replace( /^\s+|\s+$/g, '' ).split( /\r?\n/g )
+    .map( function( line ) {
+      return line.replace( /^\s+|\s+$/g, '' )
+    })
+    .sort( function( a, b ) {
+      if( a.indexOf( '*' ) === 0 ) return -1
+      return a.localeCompare( b )
+    })
+
+  if( lines.length ) {
+    lines[0] = lines[0].replace( /^\*\s+/, '' )
+  }
+
+  inquirer.prompt([{
+    type: 'list',
+    name: 'branch',
+    message: 'Select branch',
+    default: lines[0],
+    choices: lines
+  }], function( results ) {
+    exec( `git checkout '${results.branch}'`, {
+      cwd: process.cwd()
+    }, function( error, stdout, stderr ) {
+
+      // Only write out the most important part
+      process.stdout.write( '\n' )
+      process.stdout.write( stdout.toString().split( /\r?\n/g ).pop() )
+      process.stderr.write( stderr.toString().split( /\r?\n/g ).shift() )
+      process.stdout.write( '\n' )
+
+      // Print status after branch change manually,
+      // as `git checkout` doesn't do color output
+      exec( 'git status -sb', function( _, stdout, stderr ) {
+
+        process.stdout.write( '\n' )
+        process.stdout.write( stdout )
+        process.stderr.write( stderr )
+
+        process.exit( error ? error.code : 0 )
+
+      })
+
+    })
+  })
+
+})
